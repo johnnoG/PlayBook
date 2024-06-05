4f9aa100683c13236b6ca02895d31652fef85284
johnnoG <95978094+johnnoG@users.noreply.github.com>
2024-06-05 12:40:44 +0300

    seperate script to a file

---
+++ Final_Proj/mainPage.js
@@ -0,0 +1,30 @@
+function showInfo() {
+  let temp;
+  let popUp = document.getElementById("popLearn");
+  let sec1 = document.getElementById("sect1");
+  let p1 = document.createElement("p");
+  let div1 = document.createElement("div");
+  div1.appendChild(p1);
+  // div1.classList.add("displayDiv");
+
+  p1.innerText =
+    "Imagine arriving in an unfamiliar place and wanting to join a game of football or basketball. With PlayBook,you can easily find and join local games without the hassle of organizing participants.Register as a player, specify your position,preferred playing foot or hand, and other game details.PlayBook locates nearby fields and lets you sign up for games at specific times.Once enough players join, a game is automatically arranged.Players can rate each other and the facilities, creating a system that matches users by skill level. PlayBook makes it easy to enjoy spontaneous matches and connect with new people,wherever you are.";
+  p1.classList.add("displayP");
+  sec1.innerHTML = "";
+  sec1.appendChild(p1);
+  p1.addEventListener("mouseleave", closeInfo);
+}
+
+function closeInfo() {
+  let sec1 = document.getElementById("sect1");
+  sec1.innerHTML = `
+      <div class="FMDiv">Playbook</div>
+      <h3>Find.Book.Play</h3>
+      <div class="mainDiv">
+        <div><a href="#"> Sign up</a></div>
+        <div id="popLearn">
+          <a href="#" onclick="showInfo()">Learn more</a>
+        </div>
+      </div>
+    `;
+}
