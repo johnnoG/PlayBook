16d0f8a7ef1178bc6a7f0c7abe5c9da54b20879e
johnnoG <95978094+johnnoG@users.noreply.github.com>
2024-06-18 21:26:44 +0300

    fix php path

--- front-end/scripts/About_me.js
+++ front-end/scripts/About_me.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const email = "user@example.com"; // Replace with the actual user's email or obtain it dynamically
-  fetch(`../../back-end/src/api/getUserData.php?email=${email}`)
+  fetch(`../../back-end/src/services/player-profile.php?email=${email}`)
     .then((response) => response.json())
     .then((data) => {
       if (data.success) {
