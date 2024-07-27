6835db83e8a74c02453c3dcb97d229fd9eb31268
johnnoG <95978094+johnnoG@users.noreply.github.com>
2024-07-27 15:39:55 +0300

    Delete Old version

--- services/check-verification.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-if (isset($_SESSION['user_verified']) && $_SESSION['user_verified'] === true) {
-    echo json_encode(true);
-} else {
-    echo json_encode(false);
-}
